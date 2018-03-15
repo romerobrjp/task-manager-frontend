65c5a6eb6513829801ac4d1be60452893060e2f8
Romero Medeiros <romeromedeiros5@gmail.com>
2018-03-14 22:26:10 -0300

    Adicionando routes configs basicas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Router } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { TasksComponent } from  './tasks/tasks.component';
 import { TaskDetailComponent } from './tasks/task-detail/task-detail.component';
+
+// forRoot vai retornar um modulo com todas as configuracoes das rotas da app
+const ROUTES = RouterModule.forRoot([
+  {
+    path: 'tasks',
+    component: TasksComponent
+  }
+])
 
 @NgModule({
   declarations: [
@@ -19,7 +28,9 @@ import { TaskDetailComponent } from './tasks/task-detail/task-detail.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule,
+    ROUTES
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/tasks/shared/task.service.ts
+++ src/app/tasks/shared/task.service.ts
@@ -2,7 +2,15 @@ import { Injectable } from '@angular/core'; // avisa pro angular que talvez essa
 //sempre que criar classe uma classe de serviço, deve-se usar o decorator Injectable
 import { Task } from './task.model'
 
-const TASKS: Array<Task> = [];
+const TASKS: Array<Task> = [
+  { id: 1, title: 'Fazer tarefa 1' },
+  { id: 2, title: 'Fazer tarefa 2' },
+  { id: 3, title: 'Fazer tarefa 3' },
+  { id: 4, title: 'Fazer tarefa 4' },
+  { id: 5, title: 'Fazer tarefa 5' },
+  { id: 6, title: 'Fazer tarefa 6' },
+  { id: 7, title: 'Fazer tarefa 7' }
+];
 
 @Injectable()
 export class TaskService {
