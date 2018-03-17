b883789e73440903977a4325dc74460d7c9eebec
Romero Medeiros <romeromedeiros5@gmail.com>
2018-03-17 11:16:19 -0300

    Usando subscribe pra capturar os params da request

--- src/app/tasks/task-detail/task-detail.component.ts
+++ src/app/tasks/task-detail/task-detail.component.ts
@@ -1,15 +1,27 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from "@angular/router";
 
 import { Task } from '../shared/task.model';
+import { TaskService } from '../shared/task.service';
 
 @Component({
   selector: 'task-detail',
   templateUrl: './task-detail.component.html'
 })
 
-export class TaskDetailComponent {
+export class TaskDetailComponent implements OnInit {
   @Input() public task: Task;
 
-  public constructor() {
+  public constructor(
+    private taskService: TaskService,
+    private route: ActivatedRoute // este objeto entrega as informações referentes à rota que está ativa no momento da navegação
+  ) {
+  }
+
+  public ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      this.taskService.getTask(+params['id'])
+        .then(task => this.task = task)
+    })
   }
 }
