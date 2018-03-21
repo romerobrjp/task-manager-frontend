1bef4e93a13fd5f367e86bc6d05c48f5c623f730
Romero Medeiros <romeromedeiros5@gmail.com>
2018-03-20 22:10:46 -0300

    Add import Observable ao TaskDetail

--- src/app/tasks/task-detail/task-detail.component.ts
+++ src/app/tasks/task-detail/task-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
 
+import {Observable} from 'rxjs';
 import 'rxjs/add/operator/switchMap';
 
 import { Task } from '../shared/task.model';
